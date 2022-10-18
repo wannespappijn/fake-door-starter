6d061cb49ffe0410091a98c352172bc200deb637
wannespappijn <wannes.pappijn@student.howest.be>
2022-10-18 15:14:44 +0200

    added confetti

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 
+
+import JSConfetti from "../node_modules/js-confetti/dist/es/index.js";
+
+
 const init = () => {
   const $form = document.querySelector("form");
+  const jsConfetti = new JSConfetti()
+
   $form.addEventListener("submit", (event) => {
     event.preventDefault();
+
+    jsConfetti.addConfetti();
   });
 }
 
