8bf09d6ab5dc921184d7376d11e59e0e5101149e
wannespappijn <wannes.pappijn@student.howest.be>
2022-10-25 13:54:21 +0200

    changed emojis

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ const init = () => {
     event.preventDefault();
 
     jsConfetti.addConfetti({
-      emojis: ['💩', '💩', '💩', '💩', '💩', '💩'],
+      emojis: ['🥷', '	🥷', '🥷', '💩', '💩', '💩'],
     })
   });
 }
